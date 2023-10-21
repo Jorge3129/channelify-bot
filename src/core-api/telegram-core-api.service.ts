26e9c3473e9ef31ccc9004952893f1ba6d403633
Heorhii Sanchenko <jorhesancho@gmail.com>
2023-10-21 19:28:42 +0300

    feat(tg-core-api): join channel

--- src/core-api/telegram-core-api.service.ts
+++ src/core-api/telegram-core-api.service.ts
@@ -16,6 +16,19 @@ class TelegramCoreApiService {
 
     return result.chats[0];
   }
+
+  public async joinChannel(
+    client: TelegramClient,
+    inputChannel: string
+  ): Promise<any> {
+    const result: any = await client.invoke(
+      new Api.channels.JoinChannel({
+        channel: inputChannel,
+      })
+    );
+
+    return result.chats[0];
+  }
 }
 
 export const telegramCoreApiService = new TelegramCoreApiService();
