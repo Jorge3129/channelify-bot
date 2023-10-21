f60bdf0a23fe390036ccf7981bb1a76049ed4aa7
Heorhii Sanchenko <jorhesancho@gmail.com>
2023-10-21 20:11:49 +0300

    feat(tg-core-api): subscribe to channel updates

--- src/core-api/telegram-core-api.service.ts
+++ src/core-api/telegram-core-api.service.ts
@@ -1,4 +1,6 @@
 import { Api, TelegramClient } from "telegram";
+import { NewMessage, NewMessageEvent } from "telegram/events";
+import { Observable } from "rxjs";
 
 class TelegramCoreApiService {
   public async createChannel(
@@ -28,6 +30,25 @@ class TelegramCoreApiService {
     );
 
     return result.chats[0];
+  }
+
+  public getChannelNewMessageUpdates(
+    client: TelegramClient,
+    chatId: string
+  ): Observable<NewMessageEvent> {
+    return new Observable((observer) => {
+      const handler = (event: NewMessageEvent) => {
+        observer.next(event);
+      };
+
+      client.addEventHandler(
+        handler,
+        new NewMessage({
+          incoming: true,
+          chats: [chatId],
+        })
+      );
+    });
   }
 }
 
