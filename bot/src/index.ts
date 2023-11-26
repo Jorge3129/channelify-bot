c0a14521c85372ad9b60d1bc3cd7262fe97de9dc
kkpagaev <kkpagaev@gmail.com>
2023-11-26 20:12:11 +0200

    added server to bot to check if app works

--- bot/src/index.ts
+++ bot/src/index.ts
@@ -1,6 +1,7 @@
 import dataSource from "./data-source";
 import { channelBot } from "./bot/bot";
 import { telegramClientFactory } from "./core-api/telegram-client.factory";
+import Fastify from "fastify";
 
 const main = async () => {
   await dataSource.initialize();
@@ -10,7 +11,24 @@ const main = async () => {
   console.log("TelegramClientFactory initialized");
 
   channelBot.launch();
+
   console.log("Bot launched");
+  const fastify = Fastify({
+    logger: true,
+  });
+
+  // Declare a route
+  fastify.get("/", async function handler(request, reply) {
+    return { hello: "world" };
+  });
+
+  // Run the server!
+  try {
+    await fastify.listen({ port: 3000 });
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
 };
 
 main();
