d63832b6f51e6c22b61087ea587e433f2c8bc6c0
kkpagaev <kkpagaev@gmail.com>
2023-11-26 19:59:54 +0200

    deploy bot

--- bot/src/config/orm-config.ts
+++ bot/src/config/orm-config.ts
@@ -9,16 +9,25 @@ dotenv.config({
 const migrationsPath = path.join(__dirname, "../migrations/*{.ts,.js}");
 const entitiesPath = path.join(__dirname, "../**/*.entity{.ts,.js}");
 
-const ormConfig: DataSourceOptions = {
-  type: "postgres",
-  host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT ?? ""),
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  entities: [entitiesPath],
-  synchronize: false,
-  migrations: [migrationsPath],
-};
+const ormConfig: DataSourceOptions =
+  process.env.DATABASE_URL !== undefined
+    ? {
+        type: "postgres",
+        url: process.env.DATABASE_URL,
+        entities: [entitiesPath],
+        synchronize: false,
+        migrations: [migrationsPath],
+      }
+    : {
+        type: "postgres",
+        host: process.env.DB_HOST,
+        port: parseInt(process.env.DB_PORT ?? ""),
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        entities: [entitiesPath],
+        synchronize: false,
+        migrations: [migrationsPath],
+      };
 
 export default ormConfig;
