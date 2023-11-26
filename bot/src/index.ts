fb70593f4be0e4743a79228f3a47e8ec7dd06984
kkpagaev <kkpagaev@gmail.com>
2023-11-26 20:22:00 +0200

    ultrafix

--- bot/src/index.ts
+++ bot/src/index.ts
@@ -24,7 +24,7 @@ const main = async () => {
 
   // Run the server!
   try {
-    await fastify.listen({ port: 3000 });
+    await fastify.listen({ host: "0.0.0.0", port: 3000 });
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
