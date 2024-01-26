64dba66ce34d0ced1c661b7d4c27bd8cf74e13d1
Heorhii Sanchenko <jorhesancho@gmail.com>
2024-01-26 17:33:45 +0200

    foo

---
+++ bot/src/foo.ts
@@ -0,0 +1,34 @@
+class Func<T, R> {
+  constructor(private fn: (arg: T) => R) {}
+
+  ["|>"]<U>(otherFnOrConst: ((arg: R) => U) | Func<R, U>): Func<T, U> {
+    const otherFn =
+      typeof otherFnOrConst === "function"
+        ? otherFnOrConst
+        : otherFnOrConst.getFn();
+
+    return new Func((a) => otherFn(this.fn(a)));
+  }
+
+  public getFn() {
+    return this.fn;
+  }
+}
+
+const $fn = <T, R>(fn: (arg: T) => R) => new Func(fn);
+
+const numToStr = $fn((a: number) => a.toString());
+
+const IOInspect = (a: any): any => {
+  console.log(a);
+  return a;
+};
+
+const foo = numToStr["|>"](IOInspect)
+  ["|>"](Number)
+  ["|>"]((x) => x * 2)
+  ["|>"](IOInspect)
+  ["|>"]((x) => x * 3)
+  ["|>"](IOInspect);
+
+foo.getFn()(2);
